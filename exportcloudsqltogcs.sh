c0caca47f903541c33076120f11ddc1bc6c22d74
emailjessemensah@gmail.com <emailjessmensa@gmail.comm>
2023-09-25 22:20:06 +0100

    first commit

--- exportcloudsqltogcs.sh
+++ exportcloudsqltogcs.sh
@@ -1,13 +1,13 @@
 bucket_name=test-jess-data-bucket
 
 gcloud sql export csv mysql-instance-source \
-gs://$bucket_name/mysql_export/stations/20180101/stations.csv \
+gs://test-jess-data-bucket/mysql_export/stations/20180101/stations.csv \
 --database=apps_db \
 --offload \
 --query='SELECT * FROM stations WHERE station_id <= 200;'
 
 gcloud sql export csv mysql-instance-source \
-gs://$bucket_name/mysql_export/stations/20180102/stations.csv \
+gs://test-jess-data-bucket/mysql_export/stations/20180102/stations.csv \
 --database=apps_db \
 --offload \
 --query='SELECT * FROM stations WHERE station_id <= 400;'
