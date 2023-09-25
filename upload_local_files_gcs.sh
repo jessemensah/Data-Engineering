a43c88ec2129ec1a9f38b207c8164b1ffb6f2d20
emailjessemensah@gmail.com <emailjessmensa@gmail.comm>
2023-09-25 18:04:07 +0100

    another commit

--- upload_local_files_gcs.sh
+++ upload_local_files_gcs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the destination bucket name in a variable
-DESTINATION_BUCKET_NAME="test-jess-data-bucket"
+# DESTINATION_BUCKET_NAME="test-jess-data-bucket"
 
 # Copy the files from the "datasets" directory to the GCS bucket recursively
-gsutil cp datasets/* "gs://${DESTINATION_BUCKET_NAME}/from-git/"
+gsutil cp datasets/* "gs://test-jess-data-bucket/from-git/"
